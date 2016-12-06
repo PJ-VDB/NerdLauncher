c5c493ae9f85858b1bac2c0fd44bd9579268bae8
PJ-VDB <pjvandenbrande@hotmail.com>
2016-12-06 12:31:28 +0100

    Chapter 22 Challenge: Icons Added

---
+++ app/src/main/java/com/example/pieter_jan/nerdlauncher/SingleFragmentActivity.java
@@ -0,0 +1,41 @@
+package com.example.pieter_jan.nerdlauncher;
+
+/**
+ * Created by pieter-jan on 12/1/2016.
+ * An abstract class that creates a fragment
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+//        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+//    // Annotation tells Android Studio that any implementation of this method should return a valid layout resource ID
+//    @LayoutRes
+//    protected int getLayoutResId() {
+//        return R.layout.activity_masterdetail;
+//    }
+}
